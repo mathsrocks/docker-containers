a09c0881d25e8d172db8d33d2221ca027f187e43
mathsrocks <jerry.zwyang@gmail.com>
2019-10-26 11:20:07 +1100

    Add dockerfiles, requirements.txt, other related files and folder structure for building docker images

---
+++ dsml/pkg_test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import pandas_profiling as pp
+
+print(np.__version__)
+print(pd.__version__)
+print(pp.__version__)
+print(tf.__version__)
